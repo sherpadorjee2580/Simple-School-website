fbec26efa68452181965b065c881986ccc70f88f
INDRA-011 <indr.yznn.77@gmail.com>
2025-08-12 22:00:49 +0545

    Hero.jsx and Hero.css

---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>Empowering Minds, Building Futures</h1>
+        <p>
+          At EduConnect Academy, we foster a vibrant learning community where
+          curiosity thrives and every student is inspired to react their full
+          potential. Join us in shapping tommorow's leaders.
+        </p>
+        <button className="hero-button">Learn More</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
