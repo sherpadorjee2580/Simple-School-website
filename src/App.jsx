a21b5dce06e3c66d63ee5a108a5f8be909d3b267
Dorji <sherpadorjee2580@gmail.com>
2025-08-13 18:35:21 +0545

    Added a new faculty Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Nav from "./Component/Nav/Nav";
 import Hero from "./Component/Hero/Hero";
+import Faculty from "./Component/Faculty/Faculty";
 
 function App() {
   return (
     <>
       <Nav />
-
       <Hero />
+      <Faculty />
     </>
   );
 }
---
+++ src/Component/Faculty/Faculty.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Faculty.css";
+
+const Faculty = () => {
+  return (
+    <div className="faculty">
+      <div className="faculty-container">
+        <p className="faculty-title">Meet Our Dedicated Faculty</p>
+        <div className="faculty-boxContainer">
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+          <div className="faculty-box">
+            <img src="user.jpg" alt="" width={100} height={100} />
+            <span className="name">Dr. Pasang Sherpa</span>
+            <span className="title">Head of Department</span>
+          </div>
+        </div>
+        <button className="faculty-viewBtn">View All Faculty</button>
+      </div>
+    </div>
+  );
+};
+
+export default Faculty;
