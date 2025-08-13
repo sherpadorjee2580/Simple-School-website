0f275e76844d16f4aaa3338ad9ed48c88cb50ae9
Tshering592 <tsheringfunchok@gmail.com>
2025-08-13 17:23:13 +0545

    Adding Navbar

---
+++ src/Component/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h2 className='logo'>DIT</h2>
+        <ul className="navbar-links">
+          <li><a href="#">Home</a></li> 
+          <li><a href="#">Recipes</a></li> 
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <button className="login">login</button>
+      </div>
+      
+    </nav>
+  );
+};
+
+export default Nav;
