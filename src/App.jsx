a53931046b72e53a7c4a3da1de156511fbf85878
Dorji <sherpadorjee2580@gmail.com>
2025-08-13 17:39:11 +0545

    Solved the merge conflct

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './Component/Nav/Nav';
-=======
-import React from "react";
+import "./App.css";
+import Nav from "./Component/Nav/Nav";
 import Hero from "./Component/Hero/Hero";
->>>>>>> 8f8523ee48dd91976b6edaa5eaea5c5266fbc7f0
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
-      <Nav/>
-    </>
-  )
-=======
-    <div>
+      <Nav />
+
       <Hero />
-    </div>
+    </>
   );
->>>>>>> 8f8523ee48dd91976b6edaa5eaea5c5266fbc7f0
 }
 
 export default App;
