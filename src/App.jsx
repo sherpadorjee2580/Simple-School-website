990db01a08bd21f389633889ae965c4735c20696
Tshering592 <tsheringfunchok@gmail.com>
2025-08-14 16:55:02 +0545

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Nav from "./Component/Nav/Nav";
 import Hero from "./Component/Hero/Hero";
 import Faculty from "./Component/Faculty/Faculty";
+import Service from "./Component/Service/Service";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Service />
       <Faculty />
     </>
   );
---
+++ src/Component/Service/Service.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Service.css";
+
+const Faculty = () => {
+  return (
+    <div className="service">
+        <div className="service-container">
+            <p className="service-title">Our Services</p>
+            <div className="service-boxContainer">
+                <div className="service-box">
+                    <span className="serive">vbvbdhvusn</span>
+                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+                </div>
+                <div className="service-box">
+                    <span className="serive">vbvbdhvusn</span>
+                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+                </div>
+                <div className="service-box">
+                    <span className="serive">vbvbdhvusn</span>
+                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+                </div>
+                <div className="service-box">
+                    <span className="serive">vbvbdhvusn</span>
+                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+                </div>
+            </div>
+            <button className="service-viewBtn">View All Services</button>
+        </div>
+    </div>
+  );
+};
+
+export default Faculty;
