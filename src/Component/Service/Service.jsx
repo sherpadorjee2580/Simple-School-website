ddc650bec0bc79910af1950a491f9d67069f1b1a
Dorji <sherpadorjee2580@gmail.com>
2025-08-14 17:26:02 +0545

    resolved the service section

--- src/Component/Service/Service.jsx
+++ src/Component/Service/Service.jsx
@@ -4,28 +4,36 @@ import "./Service.css";
 const Faculty = () => {
   return (
     <div className="service">
-        <div className="service-container">
-            <p className="service-title">Our Services</p>
-            <div className="service-boxContainer">
-                <div className="service-box">
-                    <span className="serive">vbvbdhvusn</span>
-                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
-                </div>
-                <div className="service-box">
-                    <span className="serive">vbvbdhvusn</span>
-                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
-                </div>
-                <div className="service-box">
-                    <span className="serive">vbvbdhvusn</span>
-                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
-                </div>
-                <div className="service-box">
-                    <span className="serive">vbvbdhvusn</span>
-                    <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
-                </div>
-            </div>
-            <button className="service-viewBtn">View All Services</button>
+      <div className="service-container">
+        <p className="service-title">Our Services</p>
+        <div className="service-boxContainer">
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
+          <div className="service-box">
+            <span className="serive">vbvbdhvusn</span>
+            <span className="description">vbvbdhvusnvvsdvdfsvvsvsvds</span>
+          </div>
         </div>
+        <button className="service-viewBtn">View All Services</button>
+      </div>
     </div>
   );
 };
