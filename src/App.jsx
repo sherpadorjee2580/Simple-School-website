d210a772d4e082945d0be08a0af3416767d958e7
Dorji <sherpadorjee2580@gmail.com>
2025-08-14 17:05:16 +0545

    Solved the merge conflict error

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import "./App.css";
 import Nav from "./Component/Nav/Nav";
 import Hero from "./Component/Hero/Hero";
 import Faculty from "./Component/Faculty/Faculty";
-<<<<<<< HEAD
 import Service from "./Component/Service/Service";
-=======
 import Program from "./Component/Programs/Program";
->>>>>>> a4650ca19e43b7207cafbfc4c906b7941a4d0f3e
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
-<<<<<<< HEAD
       <Service />
-=======
       <Program />
->>>>>>> a4650ca19e43b7207cafbfc4c906b7941a4d0f3e
       <Faculty />
     </>
   );
