a4650ca19e43b7207cafbfc4c906b7941a4d0f3e
INDRA-011 <indr.yznn.77@gmail.com>
2025-08-14 12:34:09 +0545

    Programs added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Nav from "./Component/Nav/Nav";
 import Hero from "./Component/Hero/Hero";
 import Faculty from "./Component/Faculty/Faculty";
+import Program from "./Component/Programs/Program";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Program />
       <Faculty />
     </>
   );
---
+++ src/Component/Programs/Program.jsx
@@ -0,0 +1,59 @@
+import "./Program.css";
+import React from "react";
+
+function Program() {
+  return (
+    <div className="program">
+      <div className="program-container">
+        <p className="program-title">Explore Our Academic Programs</p>
+        <div className="program-boxContainer">
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+          <div className="program-box">
+            <span className="name">Early Childhood Education</span>
+            <span className="title">
+              Nurturing young minds with a play-based curriculum focusing on
+              cognitive and social-emotional development.
+            </span>
+          </div>
+        </div>
+        <button className="program-viewBtn">view all programs</button>
+      </div>
+    </div>
+  );
+}
+
+export default Program;
